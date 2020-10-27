dfc335eef461b04a9ed58de6294ec184eb64f7a4
bogdanpasterak <bogdanpasterak@gmail.com>
2020-10-27 19:20:47 +0000

    start VMC

--- index.php
+++ index.php
@@ -7,7 +7,36 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<?php
+    // Model
+    // variable
+    $deck;
+
+    function fillRandomDeck() {
+        for ($i=0; $i < 52; $i++) { 
+            $deck[$i] = $i;
+        }
+    }
+
+    function init() {
+        $deck = array();
+        fillRandomDeck();
+
+    }
+
+    // Controller
+    // start 
+    if (!isset($deck)) {
+        init();
+    }
+
+?>
     <h1>Bogdan Pasterak L00157106</h1>
     
+<?php
+    // View
+    echo "<p>Deck {$deck}</p>";
+
+?>
 </body>
 </html>
