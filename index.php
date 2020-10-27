ae4671ef5efb07a3f1cc2d284bb162d5c9b7220e
Bogdan Pasterak <bogdanpasterak@gmail.com>
2020-10-27 17:57:04 +0000

    add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blackjack</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Bogdan Pasterak L00157106</h1>
+    
+</body>
+</html>
