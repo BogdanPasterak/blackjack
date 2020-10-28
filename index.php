3fd2dd8ed24bffd90d30c1fc01358c36386e1fcc
bogdanpasterak <bogdanpasterak@gmail.com>
2020-10-28 17:43:53 +0000

    start view

--- index.php
+++ index.php
@@ -8,9 +8,20 @@
 </head>
 <body>
 <?php
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    // first time false
+    $visited = isset($_SESSION['visited']);
+    // next true
+    $_SESSION['visited'] = true;
+
     // Model
     // variable
-    $deck;
+
+    // Controller
+    // echo "Visited ".rand(1,3);
+
 
     function fillRandomDeck() {
         $randomDeck = array();
@@ -20,30 +31,52 @@
         return $randomDeck;
     }
 
-    function init() {
-        global $deck;
-        $deck = fillRandomDeck();
-    }
 
-    // Controller
-    // start 
-    if (!isset($deck)) {
-        init();
+    if (! $visited) {
+        $_SESSION["deck"] = fillRandomDeck();
+        $_SESSION["pointer"] = 0;
+    }
+    else {
+        if (! isset($_SESSION["deck"])) {
+            session_destroy();
+            echo "Destroy Session";
+        }
     }
 
 
     echo "<h1>Bogdan Pasterak L00157106</h1>";
     // View
-    // echo "<p>Deck ".count($deck)." el 3 = ".$deck[2]."</p>";
-    if (isset($_GET['cli'])) {
-        echo "cll button clicked";
+
+    if (isset($_GET['submit'])) {
+        if ($_GET['submit'] == "Reset") {
+            $_SESSION["pointer"] = 0;
+        }
+        elseif ($_GET['submit'] == "Sto") {
+            $_SESSION["pointer"] = 30;
+        }
+        else {
+            $_SESSION["pointer"]++;
+        }
     }
+
+    // echo "<p>Deck ".count($_SESSION["deck"])." el ".$_SESSION["pointer"]." = ".
+    //             $_SESSION["deck"][$_SESSION["pointer"]]."</p>";
 
 ?>
 
 <form action="index.php" method="get">
-    <input type="submit" name="cli" value="Sto">
+    <input type="submit" name="submit" value="DEAL">
+    <input type="submit" name="submit" value="HIT">
+    <input type="submit" name="submit" value="STAND">
+    <input type="submit" name="submit" value="DOUBLE">
+    <input type="submit" name="submit" value="SPLIT">
+    <input type="submit" name="submit" value="RESET">
 </form>
     
+<?php
+    print_r("Visited: ".$_SESSION["visited"]."<br>");
+    print_r("Deck: ".count($_SESSION["deck"])."<br>");
+    print_r("Pointer: ".$_SESSION["pointer"]."<br>");
+?>
 </body>
 </html>
